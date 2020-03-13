0341fbf6f59ff287f9e6624a5c2daa193e3fb2f7
unknown <godls036@gamil.com>
2020-03-13 12:23:57 +0900

    Finish pagenator

--- rooms/views.py
+++ rooms/views.py
@@ -1,21 +1,13 @@
 import math
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from . import models as rooms_models
 
 
 def all_rooms(request):
-    page = request.GET.get("page", 1)
-    page = int(page or 1)
-    page_size = 10
-    page_limit = page_size * page
-    offset = page_limit - page_size
+    page = request.GET.get("page")
+    room_list = rooms_models.Room.objects.all()
+    paginator = Paginator(room_list, 10)
+    rooms = paginator.get_page(page)
 
-    all_rooms = rooms_models.Room.objects.all()[offset:page_limit]
-    page_count = rooms_models.Room.objects.count() / page_size
-    page_count = math.ceil(page_count)
-
-    return render(
-        request,
-        "rooms/home.html",
-        context={"all_rooms": all_rooms, "page": page, "page_count": page_count, "range": range(1, page_count + 1)},
-    )
+    return render(request, "rooms/home.html", {"page": rooms})
