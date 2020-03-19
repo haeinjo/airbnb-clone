939123a4f9784a6d23541f79a662bb8c7672f7b7
godls036 <godls016@naver.com>
2020-03-19 18:30:01 +0900

    Custom Http404 Page

--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,7 @@
 from django.views.generic import ListView
-from django.shortcuts import render
+from django.http import Http404
+from django.urls import reverse
+from django.shortcuts import render, redirect
 from . import models
 
 
@@ -13,7 +15,11 @@ class HomeView(ListView):
 
 
 def room_detail(request, pk):
-    return render(request, "rooms/detail.html")
+    try:
+        room = models.Room.objects.get(pk=pk)
+        return render(request, "rooms/detail.html", {"room": room})
+    except models.Room.DoesNotExist:
+        raise Http404()
 
 
 # from django.shortcuts import render, redirect
