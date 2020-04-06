f3b6b8d52864d8502a0db41b73dcf71b528ca985
godls036 <godls016@naver.com>
2020-04-06 21:22:51 +0900

    20.11 Forms Shortcut before using looping form field trick

--- users/views.py
+++ users/views.py
@@ -48,12 +48,6 @@ class SignUpView(FormView):
     form_class = forms.SignUpForm
     success_url = reverse_lazy("core:home")
 
-    initial = {
-        "first_name": "Haein",
-        "last_name": "Jo",
-        "email": "godls036@gmail.com",
-    }
-
     def form_valid(self, form):
         form.save()
         email = form.cleaned_data.get("email")
