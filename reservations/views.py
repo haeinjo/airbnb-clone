82bcbbd00ba74ac20ddd63f0035bf8822a987ae4
godls036 <godls016@naver.com>
2020-04-18 02:23:22 +0900

    24.11 Reservation Detail part One

--- reservations/views.py
+++ reservations/views.py
@@ -1,4 +1,5 @@
 import datetime
+from django.http import Http404
 from django.views.generic import View
 from django.contrib import messages
 from django.shortcuts import render, redirect, reverse
@@ -34,5 +35,14 @@ class ReservationDetailView(View):
     def get(self, *args, **kwargs):
         pk = kwargs.get("pk")
         reservation = models.Reservation.objects.get_or_none(pk=pk)
-        if not reservation:
-            return redirect(reverse("core:home"))
+        if not reservation or (
+            reservation.guest != self.request.user
+            and reservation.room.host != self.request.user
+        ):
+            raise Http404()
+
+        return render(
+            self.request,
+            "reservations/detail.html",
+            context={"reservation": reservation},
+        )
